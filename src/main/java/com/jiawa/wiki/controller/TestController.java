b8951e5d72188dde95175c70c3db29beddd44bc1
zhangchen <2320080479@qq.com>
2021-07-19 20:39:50 +0800

    2-7 增加hello接口测试test.httpl

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -2,6 +2,8 @@ package com.jiawa.wiki.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 public class TestController {
 
@@ -9,5 +11,9 @@ public class TestController {
     public  String Hello(){
 
         return  "hello world";
+    }
+    @PostMapping(value = "/hello/post")
+    public String HelloPost(String name){
+        return "hello world Post"+name;
     }
 }
