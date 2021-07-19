ae599bff3855eadc79dfd0db51eeff44d6413ea6
zhangchen <2320080479@qq.com>
2021-07-19 19:27:52 +0800

    2-5 修改启动文案

--- src/main/java/com/jiawa/wiki/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/WikiApplication.java
@@ -1,15 +1,33 @@
 package com.jiawa.wiki;
 
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+
     public static void main(String[] args) {
 
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app=  new SpringApplication(WikiApplication.class);
+
         System.out.println("hello world" );
+        Environment env =app.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("地址：http://127.0.0.1:{}");
+        LOG.info(env.getProperty("server.port"));
+
+
+
+
+
+
+
     }
 
 }
