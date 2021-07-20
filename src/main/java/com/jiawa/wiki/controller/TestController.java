4ef24aead30ce317f6a1b01ced5615a71c6c72b5
zhangchen <2320080479@qq.com>
2021-07-20 10:42:15 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,16 +1,19 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:lulu}")
+    private String hello;
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public  String Hello(){
 
-        return  "hello world";
+        return  "hello world"+hello;
     }
     @PostMapping(value = "/hello/post")
     public String HelloPost(String name){
