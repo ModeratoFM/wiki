3b4c1d2dc3060c41c4b09e658b1d586c47f2009b
zhangchen <2320080479@qq.com>
2021-07-21 10:41:09 +0800

    3-5 集成mybatis generator-config.xml 生成demo代码表

--- doc/all.sql
+++ doc/all.sql
@@ -8,5 +8,14 @@ create table if not exists test
 )
 default charset=utf8mb4 comment '测试';
 
-insert into `test` (id, name, password) values (1,"测试","password")
+insert into `test` (id, name, password) values (1,"测试","password");
 
+
+drop table if exists `demo`;
+create table if not exists demo(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+)engine=innodb default char set =utf8mb4 comment ='测试';
+
+insert into `demo` (id, name) values (1,'测试');
